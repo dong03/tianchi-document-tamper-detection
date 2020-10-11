656859680e18db1ce5b26c271ee8d787c76aa518
Dongchengbo <dongchengbo@ruc.edu.cn>
2020-10-11 10:59:50 +0800

    add augmentation

--- dataset.py
+++ dataset.py
@@ -24,7 +24,133 @@ import dlib
 
 原本只有padding 3（原始尺寸）  修改为random.randint(3,5),三种尺度
 """
+def prepare_bit_masks(mask):
+    h, w = mask.shape
+    mid_w = w // 2
+    mid_h = w // 2
+    masks = []
+    ones = np.ones_like(mask)
+    ones[:mid_h] = 0
+    masks.append(ones)
+    ones = np.ones_like(mask)
+    ones[mid_h:] = 0
+    masks.append(ones)
+    ones = np.ones_like(mask)
+    ones[:, :mid_w] = 0
+    masks.append(ones)
+    ones = np.ones_like(mask)
+    ones[:, mid_w:] = 0
+    masks.append(ones)
+    ones = np.ones_like(mask)
+    ones[:mid_h, :mid_w] = 0
+    ones[mid_h:, mid_w:] = 0
+    masks.append(ones)
+    ones = np.ones_like(mask)
+    ones[:mid_h, mid_w:] = 0
+    ones[mid_h:, :mid_w] = 0
+    masks.append(ones)
+    return masks
 
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+
+def blackout_convex_hull(img):
+    try:
+        out_img = img.copy()
+        rect = detector(out_img)[0]
+        sp = predictor(out_img, rect)
+        landmarks = np.array([[p.x, p.y] for p in sp.parts()])
+        outline = landmarks[[*range(17), *range(26, 16, -1)]]
+        Y, X = skimage.draw.polygon(outline[:, 1], outline[:, 0])
+        cropped_img = np.zeros(out_img.shape[:2], dtype=np.uint8)
+        cropped_img[Y, X] = 1
+
+        y, x = measure.centroid(cropped_img)
+        y = int(y)
+        x = int(x)
+
+        first = random.random() > 0.5
+        if random.random() > 0.5:
+            if first:
+                cropped_img[:y, :] = 0
+            else:
+                cropped_img[y:, :] = 0
+        else:
+            if first:
+                cropped_img[:, :x] = 0
+            else:
+                cropped_img[:, x:] = 0
+
+        out_img[cropped_img > 0] = 0
+        return out_img
+    except Exception as e:
+        return img
+
+
+def dist(p1, p2):
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+
+def remove_eyes(image, landmarks):
+    image = image.copy()
+    (x1, y1), (x2, y2) = landmarks[:2]
+    mask = np.zeros_like(image[..., 0])
+    line = cv2.line(mask, (x1, y1), (x2, y2), color=(1), thickness=2)
+    w = dist((x1, y1), (x2, y2))
+    dilation = int(w // 4)
+    line = binary_dilation(line, iterations=dilation)
+    image[line, :] = 0
+    return image
+
+
+def remove_nose(image, landmarks):
+    image = image.copy()
+    (x1, y1), (x2, y2) = landmarks[:2]
+    x3, y3 = landmarks[2]
+    mask = np.zeros_like(image[..., 0])
+    x4 = int((x1 + x2) / 2)
+    y4 = int((y1 + y2) / 2)
+    line = cv2.line(mask, (x3, y3), (x4, y4), color=(1), thickness=2)
+    w = dist((x1, y1), (x2, y2))
+    dilation = int(w // 4)
+    line = binary_dilation(line, iterations=dilation)
+    image[line, :] = 0
+    return image
+
+
+def remove_mouth(image, landmarks):
+    image = image.copy()
+    (x1, y1), (x2, y2) = landmarks[-2:]
+    mask = np.zeros_like(image[..., 0])
+    line = cv2.line(mask, (x1, y1), (x2, y2), color=(1), thickness=2)
+    w = dist((x1, y1), (x2, y2))
+    dilation = int(w // 3)
+    line = binary_dilation(line, iterations=dilation)
+    image[line, :] = 0
+    return image
+
+
+def remove_landmark(image, landmarks):
+    #landmarks[left_eye_x,left_eye_y,right_eye_x,right_eye_y,
+    if random.random() > 0.5:
+        image = remove_eyes(image, landmarks)
+    elif random.random() > 0.5:
+        image = remove_mouth(image, landmarks)
+    elif random.random() > 0.5:
+        image = remove_nose(image, landmarks)
+    return image
+
+
+def change_padding(image, part=5):
+    out = image.copy()
+    h, w = out.shape[:2]
+    # original padding was done with 1/3 from each side, too much
+    pad_h = int(((3 / 5) * h) / part)
+    pad_w = int(((3 / 5) * w) / part)
+    out = out[h // 5 - pad_h:-h // 5 + pad_h, w // 5 - pad_w:-w // 5 + pad_w]
+    return out
 
 class DeepFakeClassifierDataset(Dataset):
 
@@ -76,6 +202,15 @@ class DeepFakeClassifierDataset(Dataset):
             else:
                 mask = np.zeros_like(image).astype(np.uint8)
 
+            if self.mode == "train" and self.hardcore and not self.rotation:
+                landmark_path = img_path.split('.')[0] + '.json'
+                # 0.7的概率随机去除landmarks，done
+                if os.path.exists(landmark_path) and random.random() < 0.75:
+                    landmarks = self.parse_json(landmark_path)
+                    image = remove_landmark(image, landmarks)
+                # 0.2的概率去除整张脸
+                elif random.random() < 0.25:
+                    image = blackout_convex_hull(image)
             rotation = 0
             if self.transforms:
                 data = self.transforms(image=image,mask=mask)
