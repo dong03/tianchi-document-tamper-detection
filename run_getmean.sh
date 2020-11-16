a68f377abf0f870d067f4f9217809d78cb87b925
chenxinru <chen_xinru1999@163.com>
2020-11-16 15:59:37 +0800

    update

--- run_getmean.sh
+++ run_getmean.sh
@@ -1,5 +1,4 @@
-clear
 rlaunch --cpu=4 --gpu=0 --memory=4096 -- python3 getmean.py \
 --th $1 \
---ori_img_dir /data/dongchengbo/test_image \
---save_dir /data/dongchengbo/xception_0.6_th
+--ori_img_dir /data/dongchengbo/tianchi_draw/res_stride8_320 \
+--save_dir /data/chenxinru/VisualSearch/tianchi_s2/s2_data/output/res_stride8_320/images
