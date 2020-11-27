d47e036e44db652a40662df6456d2323d8b6b6c5
Dongchengbo <dongchengbo@ruc.edu.cn>
2020-11-27 09:17:23 +0800

    fix

--- train.sh
+++ train.sh
@@ -1,7 +1,8 @@
 clear
-cd ./train
+python3 ../common/global.py --config model1
 rlaunch --cpu=4 --gpu=2 --memory=8192 -- python3 train.py --gpu_id -1 --gpu_num 2 --config model1
-#python3 train.py --gpu_id -1 --gpu_num 2 --config model2
+python3 ../common/global.py --config model2
+python3 train.py --gpu_id -1 --gpu_num 2 --config model2
 #python3 train.py --gpu_id -1 --gpu_num 2 --config model3
 #python3 train.py --gpu_id -1 --gpu_num 2 --config model4
 #python3 train.py --gpu_id -1 --gpu_num 2 --config model5
